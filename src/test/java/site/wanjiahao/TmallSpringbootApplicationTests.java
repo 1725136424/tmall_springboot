1f1e968c78a8bd976eb35709ff4c1878ecc471dd
wanjiahao <1725136424@qq.com>
2020-09-13 17:19:05 +0800

    初始化项目

---
+++ src/test/java/site/wanjiahao/TmallSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package site.wanjiahao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TmallSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
